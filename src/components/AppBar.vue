211cace259e4383f3d42f2bf756d01cb6a8a8358
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2020-08-16 07:27:26 +0300

    initial commit

---
+++ src/components/AppBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app-bar app color="blue" dark>
+    <v-spacer></v-spacer>
+
+    <!-- Links -->
+    <v-btn href="#" text>
+      About
+    </v-btn>
+
+    <v-btn href="#" text>
+      Projects
+    </v-btn>
+    <v-btn href="#" text>
+      Contact Me
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppBar",
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-row class="text-center">
+    <v-col class="mb-4">
+      <h1 class="display-2 font-weight-bold mb-3">
+        Ignatius Mbugua
+      </h1>
+
+      <h2 class="subheading font-weight-regular">
+        Web Developer
+      </h2>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  name: "Header.vue",
+};
+</script>
