f772e7ba53d00a66ec076b9a62f81c38a8048047
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2020-08-18 14:52:21 +0300

    add alignment and image

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,14 +1,19 @@
 <template>
   <section class="mb-4">
-    <v-row class="text-center">
-      <v-col>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Ignatius Mbugua
-        </h1>
+    <v-row class="align-md-center">
+      <v-col cols="12" sm="6">
+        <div class="text-center">
+          <h1 class="display-2 font-weight-bold mb-3">
+            Ignatius Mbugua
+          </h1>
 
-        <h2 class="subheading font-weight-regular">
-          Web Developer
-        </h2>
+          <h3 class="subheading font-weight-regular">
+            Web Developer
+          </h3>
+        </div>
+      </v-col>
+      <v-col cols="12" sm="6">
+        <v-img src="../assets/undraw_dev_productivity.svg"></v-img>
       </v-col>
     </v-row>
   </section>
