47664cc24f14d436014f8206b8e42578c64e9a61
Ignatius Mbugua <ignatiusmbugua21@gmail.com>
2020-08-19 06:58:50 +0300

    add images

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -2,12 +2,12 @@
   <section>
     <h1 class="text-center">PORTFOLIO</h1>
     <v-row>
-      <v-col cols="12" sm="12" md="4">
+      <v-col cols="12" sm="12" md="4" lg="3">
         <v-card class="rounded-lg">
           <v-img
             class="white--text align-end"
             height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            src="../assets/patient_medical_drug_prescription.jpg"
           >
             <v-card-title>Lorem ipsum</v-card-title>
           </v-img>
@@ -32,12 +32,12 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="12" sm="12" md="4">
+      <v-col cols="12" sm="12" md="4" lg="3">
         <v-card class="rounded-lg">
           <v-img
             class="white--text align-end"
             height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            src="../assets/doctor_drug_prescription.jpg"
           >
             <v-card-title>Lorem ipsum</v-card-title>
           </v-img>
@@ -62,12 +62,42 @@
           </v-card-actions>
         </v-card>
       </v-col>
-      <v-col cols="12" sm="12" md="4">
+      <v-col cols="12" sm="12" md="4" lg="3">
         <v-card class="rounded-lg">
           <v-img
             class="white--text align-end"
             height="200px"
-            src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+            src="../assets/compressed_projects_course.png"
+          >
+            <v-card-title>Lorem ipsum</v-card-title>
+          </v-img>
+
+          <v-card-text class="text--primary">
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Deleniti
+              consequuntur hic cupiditate enim dolore, est, quaerat consectetur
+              odio dolores quae recusandae vero. Ex, ab. Recusandae maiores
+              atque odio pariatur possimus?
+            </div>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn color="blue" text>
+              View
+            </v-btn>
+
+            <v-btn color="blue" text>
+              Link
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12" sm="12" md="4" lg="3">
+        <v-card class="rounded-lg">
+          <v-img
+            class="white--text align-end"
+            height="200px"
+            src="../assets/compressed_projects_nanotech.png"
           >
             <v-card-title>Lorem ipsum</v-card-title>
           </v-img>
